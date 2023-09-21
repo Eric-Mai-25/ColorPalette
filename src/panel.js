7cde58e82756aca42f85d3db293062b2f656e5b8
Eric-Mai-25 <ericmai2@gmail.com>
2023-09-21 09:06:17 -0700

    Finilized all functionality besides preview

--- src/panel.js
+++ src/panel.js
@@ -73,7 +73,7 @@ export default class Panel {
     lockIcon.classList.add("lock")
 
     // lockIcon.classList.add('lockIcon')
-    let imgSrc = this.lock ? '/src/styles/lock/lock.svg' : "/src/styles/unlock/unlock.svg"
+    let imgSrc = this.lock ? 'src/styles/lock/lock.svg' : "src/styles/unlock/unlock.svg"
     lockIcon.src = imgSrc
     
     const lockUp = document.getElementById(`${this.color}`);
